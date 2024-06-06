58922c08132a661a894a77d20f51f08c1004467c
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-06-06 10:09:32 +0000

    first commit

---
+++ src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/Application.java
@@ -0,0 +1,13 @@
+package dgi.dic2.a4l0u_c0d3.toppou20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dgi/dic2/a4l0u_c0d3/toppou20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dgi.dic2.a4l0u_c0d3.toppou20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
