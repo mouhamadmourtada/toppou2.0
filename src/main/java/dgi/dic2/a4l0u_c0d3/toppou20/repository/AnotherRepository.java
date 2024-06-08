13e12aa9c035b5725b0dff0373f0555b1939e490
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-06-08 01:42:11 +0000

    enfin

--- src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/AnotherRepository.java
+++ src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/AnotherRepository.java
@@ -2,14 +2,15 @@ package dgi.dic2.a4l0u_c0d3.toppou20.repository;
 
 import dgi.dic2.a4l0u_c0d3.toppou20.model.Todo;
 import dgi.dic2.a4l0u_c0d3.toppou20.projection.CompleteTodo;
-import dgi.dic2.a4l0u_c0d3.toppou20.projection.NoCategoryTodo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-@RepositoryRestResource(collectionResourceRel = "todos", path = "persons"
-        , excerptProjection = CompleteTodo.class
-)
+//@RepositoryRestResource(collectionResourceRel = "todos", path = "persons"
+//        , excerptProjection = CompleteTodo.class
+
+//)
+@RepositoryRestResource(collectionResourceRel = "todos", path = "persons", excerptProjection = CompleteTodo.class)
 public interface AnotherRepository extends CrudRepository<Todo, Long> {
 }
