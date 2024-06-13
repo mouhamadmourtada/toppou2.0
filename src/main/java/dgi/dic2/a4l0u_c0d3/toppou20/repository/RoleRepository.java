730ceb584bd34e3059d29ff7e5fb9697fae67732
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-06-13 00:22:31 +0000

    cors en fin

--- src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/RoleRepository.java
+++ src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import dgi.dic2.a4l0u_c0d3.toppou20.model.Role;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
