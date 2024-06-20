fc346fa66d3ee7b25ce1fb1b1fc1251772d3fc9e
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-06-20 14:53:24 +0000

    solution pour entité associées

--- src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/UserRepository.java
+++ src/main/java/dgi/dic2/a4l0u_c0d3/toppou20/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package dgi.dic2.a4l0u_c0d3.toppou20.repository;
 
 
+import dgi.dic2.a4l0u_c0d3.toppou20.dto.UserWithRoleDTO;
 import dgi.dic2.a4l0u_c0d3.toppou20.model.User;
+import dgi.dic2.a4l0u_c0d3.toppou20.model.Role;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,15 +15,26 @@ import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource
+//@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
     public Optional<User> findByUsername(String username);
 
-    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.libelle = :roleName")
-    List<User> findByRoleName(@Param("roleName") String roleName);
+//    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.libelle = :roleName")
+//    List<User> findByRoleName(@Param("roleName") String roleName);
 
 
-    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.libelle IN :roleNames")
-    List<User> findByRoles(@Param("roleNames") List<String> roleNames);
+//    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.libelle IN :roleNames")
+//    List<User> findByRoles(@Param("roleNames") List<String> roleNames);
+
+
+
+//    @EntityGraph(attributePaths = { "roles" })
+//    Optional<User> findById(Long id);
+
+
+//    @Override
+//    @EntityGraph(attributePaths = {"roles"})
+//    List<User> findAll();
+
 }
